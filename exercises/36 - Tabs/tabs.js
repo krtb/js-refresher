30bab0a9fde33be6810017c5c65c731692c955c2
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-14 18:28:56 -0400

    loop over buttons, add click callback

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,13 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = document.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(event) {
+    console.log(event.currentTarget);
+}
+
+tabButtons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        console.log(`clicked the ${button.innerText} button!`)
+    })
+});
