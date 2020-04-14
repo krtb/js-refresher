9ab101a0654b8fe3f98cbcde984cd6b700b747d1
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-14 19:33:01 -0400

    get tab panel, set hidden to false

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,6 +1,8 @@
 const tabs = document.querySelector('.tabs');
 const tabButtons = document.querySelectorAll('[role="tab"]');
-const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+// need to turn NODE LIST into an ARRAY to use find() method
+// solution => Array.from();
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
 
 function handleTabClick(event) {
 
@@ -16,11 +18,12 @@ function handleTabClick(event) {
     // Method 1
     event.currentTarget.setAttribute('aria-selected', true);
     const { id } = event.currentTarget
-    const tabPanel = document.querySelector(`[aria-labelledby=${id}]`);
-    tabPanel.hidden = false;
+    // const tabPanel = document.querySelector(`[aria-labelledby=${id}]`);
+    // tabPanel.hidden = false;
 
     // Method 2
-
+    const tabPanel = tabPanels.find((panel)=> panel.getAttribute('aria-labelledby') === id );    
+    tabPanel.hidden = false;
 }
 
 tabButtons.forEach(button => {
