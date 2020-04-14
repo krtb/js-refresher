6373af0865d15243a8626dcf28895a220cca4bd3
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-14 18:16:45 -0400

    set button disabled prop to false when on screen

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,10 +1,22 @@
 const terms = document.querySelector('.terms-and-conditions');
 const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
 
 function obCallback(payload) {
-    console.log(payload);
+    console.log(payload[0].intersectionRatio);
+    if (payload[0].intersectionRatio === 1) {
+        button.disabled = false;
+        console.log('removing...');
+        ob.unobserve(terms.lastElementChild)
+    }else {
+        button.disabled = true;
+    }
 }
 
-const ob = new IntersectionObserver(obCallback);
+const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
+});
 
-ob.observe(watch);
+// ob.observe(watch);
+ob.observe(terms.lastElementChild);
