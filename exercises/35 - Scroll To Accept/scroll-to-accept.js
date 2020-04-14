9d9ef5c53e69a196b44a9440a5b127f63477d1b2
Kurt Bauer <kurtbauer55@protonmail.com>
2020-04-14 17:45:07 -0400

    create IntersectionObserver instance, to observe e

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,10 @@
-console.log('IT WORKS!');
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+
+function obCallback(payload) {
+    console.log(payload);
+}
+
+const ob = new IntersectionObserver(obCallback);
+
+ob.observe(watch);
